152472adf23988dd1042e3a57365385ac35b87e5
Garipov <garipovv98@gmail.com>
2023-10-30 22:49:14 +0300

    postgre connect

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+from decouple import config
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',     
+        'USER': 'postgres',   
+        'PASSWORD': config('DATABASE_PASSWORD'), 
+        'HOST': 'localhost',      
+        'PORT': '5432',              
     }
 }
 
